ae86cb8401b57115dee4c29b72b86d1b6a392bf0
d3vkk <donaldkk22@yahoo.com>
2021-08-12 15:58:02 +0300

    refactor: Replace HelloWorld with LandingPage

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-gray-200">Vite Vue3 Tailwind Starter</div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+
+<style scoped>
+</style>
