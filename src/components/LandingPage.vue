ff35bc6eb6f3ea372e2f0866c04103d7f14353dc
d3vkk <donaldkk22@yahoo.com>
2021-08-12 16:19:19 +0300

    refactor: Update LandingPage content

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="text-gray-200">Vite Vue3 Tailwind Starter</div>
+  <div>
+    <div class="text-4xl font-bold">Vue3 Tailwind Starter</div>
+  </div>
 </template>
 
 <script>
