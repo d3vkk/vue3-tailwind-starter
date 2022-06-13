de63511b8cbcd9c4281d4226f77b9f9d074d0f55
d3vkk <donaldkk22@yahoo.com>
2022-06-13 09:36:13 +0300

    chore: Move router to own folder

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import router from "./router/index";
 import store from "./store/index";
 import App from "./App.vue";
-import LandingPage from "./views/LandingPage.vue";
 import "./assets/css/index.css";
 
 const app = createApp(App);
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: LandingPage,
-    },
-  ],
-});
-
 app.use(router);
 app.use(store);
 app.mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LandingPage from "../views/LandingPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: LandingPage,
+    },
+  ],
+});
+
+export default router;
